f47893d3b16594ccd1b02a894bcb500cbf570639
Matheus Santana <matheus.santana@b2wdigital.com>
2018-12-17 20:02:31 -0200

    Links da Tarefa XML

--- primeiro.go
+++ primeiro.go
@@ -1,54 +1,108 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"io"
-	"io/ioutil"
-	"os"
-
-	"github.com/kr/pretty"
-	"gitlab.b2w/team/alpha/omega.git/aggregation/sitemanager"
+	"net/http"
 )
 
-type AnywayReader struct {
-	reader io.Reader
-}
+// ///package main
 
-func (r *AnywayReader) FetchContent(context.Context) (content []byte, err error) {
-	return ioutil.ReadAll(r.reader)
-}
+// import (
+// 	"context"
+// 	"fmt"
+// 	"io"
+// 	"io/ioutil"
+// 	"os"
 
-func NewAnywayReader(r io.Reader) *AnywayReader {
-	return &AnywayReader{r}
+// 	"github.com/kr/pretty"
+// 	"gitlab.b2w/team/alpha/omega.git/aggregation/sitemanager"
+// )
+
+// type AnywayReader struct {
+// 	reader io.Reader
+// }
+
+// func (r *AnywayReader) FetchContent(context.Context) (content []byte, err error) {
+// 	return ioutil.ReadAll(r.reader)
+// }
+
+// func NewAnywayReader(r io.Reader) *AnywayReader {
+// 	return &AnywayReader{r}
+// }
+
+func IsEmpty(data string) bool {
+	if len(data) == 0 {
+		return true
+	} else {
+		return false
+	}
 }
 
 func main() {
-	// Open our xmlFile
-	xmlFile, err := os.Open("/home/msantanaz/Downloads/menu.xml")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer xmlFile.Close()
 
-	anywayreader := NewAnywayReader(xmlFile)
+	uName, email, pwd, pwdConfirm := "", "", "", ""
 
-	_ = anywayreader
+	mux := http.NewServeMux()
 
-	sm, err := sitemanager.NewClient(
-		sitemanager.SetObjectFetcher(anywayreader),
-	)
-	if err != nil {
-		panic(err)
-	}
+	// Signup
+	mux.HandleFunc("/signup", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
 
-	cc := sitemanager.NewClientCategory(sm)
+		/*
+		   // Available for testing.
+		   for key, value := range r.Form {
+		       fmt.Printf("%s = %s\n", key, value)
+		   }
+		*/
 
-	aggs, err := cc.FetchByCategoryId(context.Background(), "443456")
-	if err != nil {
-		panic(err)
-	}
+		uName = r.FormValue("username")     // Data from the form
+		email = r.FormValue("email")        // Data from the form
+		pwd = r.FormValue("password")       // Data from the form
+		pwdConfirm = r.FormValue("confirm") // Data from the form
 
-	fmt.Println(aggs)
-	pretty.Println(aggs)
+		// Empty data checking
+		uNameCheck := IsEmpty(uName)
+		emailCheck := IsEmpty(email)
+		pwdCheck := IsEmpty(pwd)
+		pwdConfirmCheck := IsEmpty(pwdConfirm)
+
+		if uNameCheck || emailCheck || pwdCheck || pwdConfirmCheck {
+			fmt.Fprintf(w, "ErrorCode is -10 : There is empty data.")
+			return
+		}
+
+		if pwd == pwdConfirm {
+			// Save to database (username, email and password)
+			fmt.Fprintln(w, "Registration successful.")
+		} else {
+			fmt.Fprintln(w, "Password information must be the same.")
+		}
+	})
+
+	// Login
+	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+
+		email = r.FormValue("email")  // Data from the form
+		pwd = r.FormValue("password") // Data from the form
+
+		// Empty data checking
+		emailCheck := IsEmpty(email)
+		pwdCheck := IsEmpty(pwd)
+
+		if emailCheck || pwdCheck {
+			fmt.Fprintf(w, "ErrorCode is -10 : There is empty data.")
+			return
+		}
+
+		dbPwd := "1234!*."                   // DB simulation
+		dbEmail := "cihan.ozhan@hotmail.com" // DB simulation
+
+		if email == dbEmail && pwd == dbPwd {
+			fmt.Fprintln(w, "Login succesful!")
+		} else {
+			fmt.Fprintln(w, "Login failed!")
+		}
+	})
+	http.ListenAndServe(":8080", mux)
 }
---
+++ web.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func index_handler(w http.ResponseWriter, r *http.Request) {
+	// a := 2
+	// b := 3
+	// c := a + b
+	// fmt.Fprintf(w, "Numero %d", c)
+	fmt.Fprintf(w, "<h1>Titulo</h1>")
+	fmt.Fprintf(w, "<p>Titulo</p>")
+
+}
+
+func value_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Number")
+}
+
+func main() {
+	http.HandleFunc("/", index_handler)
+	http.HandleFunc("/continue", value_handler)
+	http.ListenAndServe(":8000", nil)
+}
